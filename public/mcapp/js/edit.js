7c71b1ece47e1dfab479601404fc0f4b8c328a01
ntavora <ntavora@devsutd.com>
2020-05-18 19:23:59 -0300

    read JSONParameters value

--- public/mcapp/js/edit.js
+++ public/mcapp/js/edit.js
@@ -528,7 +528,7 @@ function fillUi(data) {
     $('#linkName').val(data.LinkName);
     $('#baseURL').val(data.BaseURL);
     $('#JSONParameter').val(data.JSONParameter);
-    JSONParameter = data.JSONParameter;
+    JSONParameter = data.JSONParameters;
     if (data.CustomParameters !== undefined || data.CustomParameters !== '') {
         $('#customParameters').val(data.CustomParameters);
     }
