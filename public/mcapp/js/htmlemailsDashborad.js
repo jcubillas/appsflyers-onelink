9c64985824913578bfc5fd7248a8823d83c7e7f4
ntavora <ntavora@devsutd.com>
2020-06-01 18:49:23 -0300

    demo links

--- public/mcapp/js/htmlemailsDashborad.js
+++ public/mcapp/js/htmlemailsDashborad.js
@@ -118,71 +118,39 @@ function getUrlParameters() {
 }
 
 
-function buildDashboard(links, from, page) {
+function buildDashboard(emails, from, page) {
   let table = '<div class="slds-lookup" data-select="multi" data-scope="single" data-typeahead="true">';
   table += '<table class="slds-table slds-table_cell-buffer slds-no-row-hover slds-table_bordered slds-table_fixed-layout" role="grid" >';
 
   table += '<tr>';
 
-  table += '<td class="header-dashboard" role="gridcell" scope="col" colspan="2"><b>OneLink Name</b></td>';
-  table += '<td class="header-dashboard" role="gridcell" scope="col" colspan="2"><b>Campaign</b></td>';
-  table += '<td class="header-dashboard" role="gridcell" scope="col" colspan="3"><b>Full URL</b></td>';
-  table += '<td class="header-dashboard" role="gridcell" scope="col" style="text-align:center;"><b># of Contents</b></td>';
-  table += '<td class="header-dashboard" role="gridcell" scope="col" ><b>Created</b></td>';
-  table += '<td class="header-dashboard" role="gridcell" scope="col" ><b>Modified</b></td>';
-  table += '<td class="header-dashboard" role="gridcell" scope="col" ></td>';
+  table += '<td class="header-dashboard" role="gridcell" scope="col" colspan="2"><b>Email Name</b></td>';
+  table += '<td class="header-dashboard" role="gridcell" scope="col" colspan="2"><b>Email type</b></td>';
+  table += '<td class="header-dashboard" role="gridcell" scope="col" colspan="3"><b>Owner</b></td>';
   table += '</tr>';
 
-  if (links !== undefined) {
-    links.sort((a, b) => ((new Date(a.Modified) < new Date(b.Modified)) ? 1 : ((new Date(b.Modified) < new Date(a.Modified)) ? -1 : 0)));
+  if (emails !== undefined) {
+ /*   emails.sort((a, b) => ((new Date(a.Modified) < new Date(b.Modified)) ? 1 : ((new Date(b.Modified) < new Date(a.Modified)) ? -1 : 0)));
     if (from == "init" || page == 1) {
-      links = links.splice(0, 15);
+      emails = emails.splice(0, 15);
       $("#currentDashboard").val(15);
     }
     else {
       var currentDashboard = $("#currentDashboard").val();
       links = links.splice(currentDashboard, 15);
       $("#currentDashboard").val(currentDashboard + 15);
-    }
+    }*/
 
-    for (let index = 0; index < links.length; index++) {
-      const element = links[index];
-      var Campaign = getCampaign(element);
+    for (let index = 0; index < emails.length; index++) {
+      const element = emails[index];
+       table += '<tr>';
 
-      table += '<tr>';
-
-      table += `<td role="gridcell" colspan="2"><div class="slds-truncate" >${element.LinkName}</div></td>`;
-      table += `<td role="gridcell" colspan="2"><div class="slds-truncate" >${Campaign}</div></td>`;
-      table += `<td role="gridcell" colspan="3"><div class="slds-truncate" title="${element.FullURL}">${element.FullURL}</div></td>`;
-      table += `<td role="gridcell"><div class="slds-truncate" style="text-align:center;">${element.ContentsCount}</div></td>`;
-      table += `<td role="gridcell"><div class="slds-truncate" >${element.Created}</div></td>`;
-      table += `<td role="gridcell"><div class="slds-truncate" >${element.Modified}</div></td>`;
-      table += '<td>';
-      table += `<div id="onelink-trigger${element.LinkID}" class="slds-dropdown-trigger slds-dropdown-trigger_click" style="padding-left:50%;">`;
-      table += '<button class="slds-button slds-button_icon slds-button_icon-border-filled" >';
-      table += '<svg class="slds-button__icon" aria-hidden="true">';
-      table += '<use xlink:href="/mcapp/images/symbols.svg#down">';
-      table += '</use>';
-      table += '</svg>';
-      table += '</button>';
-      table += '<div class="slds-dropdown slds-dropdown_left" style="margin-top: -1px!important">';
-      table += '<ul class="slds-dropdown__list" role="menu" >';
-      table += '<li class="slds-dropdown__item" role="presentation">';
-      table += `<a href="/dashboard/edit/?lid=${element.LinkID}&eid={0}&rt={1}" class="edit" id="edit${index}" role="menuitem" tabindex="0">`;
-      table += '<span class="slds-truncate" title="Edit">Edit</span>';
-      table += '</a></li>';
-      table += '<li class="slds-dropdown__item" role="presentation">';
-      table += `<a href="#" onclick="Duplicate(${element})" class="Duplicate" id="Duplicate${index}" role="menuitem" tabindex="0">`;
-      table += '<span class="slds-truncate" title="Duplicate">Duplicate</span>';
-      table += '</a></li>';
-      table += '</ul>';
-      table += '</div>';
-      table += '</div>';
-      table += '</div>';
-      table += '</td>';
+      table += `<td role="gridcell" colspan="2"><div class="slds-truncate" ><a href="#" id="link${index}" "${element.name}</div></td>`;
+      table += `<td role="gridcell" colspan="2"><div class="slds-truncate" >${element.assetType.name}</div></td>`;
+      table += `<td role="gridcell" colspan="3"><div class="slds-truncate" title="${element.owner.name}">${element.owner.name}</div></td>`;
       table += '</tr>';
     }
-  }
+  } 
   table += '</table>';
   table += '</div>';
 
@@ -191,11 +159,12 @@ function buildDashboard(links, from, page) {
 
   ready();
 
-  for (let index = 0; index < links.length; index++) {
-    const element = links[index];
-    document.getElementById(`Duplicate${index}`).addEventListener("click", function (e) {
+  for (let index = 0; index < emails.length; index++) {
+    const element = emails[index];
+    document.getElementById(`link${index}`).addEventListener("click", function (e) {
       e.preventDefault();
-      Duplicate(element);
+      var links = getLinks(element.views.html.content);
+       console.log(links);
     });
   }
 }
@@ -236,7 +205,10 @@ function loadHtmlEmails(urlParams, from, page) {
     },
     data: postData,
     success: (data) => {
-      console.log(data);
+      var emails = data.body;
+            $('#rt').val(data.refresh_token);
+            replaceUrlTOkens($('#rt').val());
+            buildDashboard(emails.items, from, page);
     },
     error(jqXHR, error, errorThrown) {
       const sat = '<div class="slds-lookup" data-select="multi" data-scope="single" data-typeahead="true"><table class="slds-table slds-table_cell-buffer slds-no-row-hover slds-table_bordered slds-table_fixed-layout" role="grid" ><thead><tr class="slds-line-height_reset"><th scope="col" colspan="2"><b>OneLink Name</b></th><th scope="col" colspan="2"><b>Full URL</b></th><th scope="col" ><b>URL</b></th><th scope="col" ><b># of Contents</b></th><th scope="col" ><b>Parameters</b></th><th scope="col" ><b>Custom Parameters</b></th><th scope="col" ><b>Created</b></th><th scope="col" ><b>Modified</b></th><th scope="col" ></th></tr></thead><tbody><tr><td role="gridcell" colspan="2"><div class="slds-truncate" >Security Review Test</div></td><td role="gridcell"  colspan="2"><div class="slds-truncate" title="https://af-esp.onelink.me/mYu3?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review&af_dp=esp%3A%2F%2Fdeeplink">https://af-esp.onelink.me/mYu3?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review&af_dp=esp%3A%2F%2Fdeeplink</div></td><td role="gridcell"><div class="slds-truncate" >https://af-esp.onelink.me/mYu3</div></td><td role="gridcell"><div class="slds-truncate" >1</div></td><td role="gridcell"><div class="slds-truncate" >?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review</div></td><td role="gridcell"><div class="slds-truncate" >&af_dp=esp%3A%2F%2Fdeeplink</div></td><td role="gridcell"><div class="slds-truncate" >2/10/2020 6:27:08 PM</div></td><td role="gridcell"><div class="slds-truncate" >2/10/2020 6:49:00 PM</div></td><td><div id="onelink-trigger274acd92-fa1a-4e81-b55d-ef663a8685c1" class="slds-dropdown-trigger slds-dropdown-trigger_click"><button class="slds-button slds-button_icon slds-button_icon-border-filled" aria-haspopup="true"><svg class="slds-button__icon" aria-hidden="true"><use xlink:href="/mcapp/images/symbols.svg#down"></use></svg><span class="slds-assistive-text">Show More</span></button><div class="slds-dropdown slds-dropdown_left"><ul class="slds-dropdown__list" role="menu" aria-label="Show More"><li class="slds-dropdown__item" role="presentation"><a href="/dashboard/edit/?lid=274acd92-fa1a-4e81-b55d-ef663a8685c1&eid={0}&rt={1}" class="edit" id="edit0" role="menuitem" tabindex="0"><span class="slds-truncate" title="Edit">Edit</span></a></li></ul></div></div></td></tr><tr><td role="gridcell" colspan="2"><div class="slds-truncate" >Security Review Test</div></td><td role="gridcell"  colspan="2"><div class="slds-truncate" title="https://af-esp.onelink.me/mYu3?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review&af_dp=esp%3A%2F%2Fdeeplink">https://af-esp.onelink.me/mYu3?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review&af_dp=esp%3A%2F%2Fdeeplink</div></td><td role="gridcell"><div class="slds-truncate" >https://af-esp.onelink.me/mYu3</div></td><td role="gridcell"><div class="slds-truncate" >1</div></td><td role="gridcell"><div class="slds-truncate" >?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review</div></td><td role="gridcell"><div class="slds-truncate" >&af_dp=esp%3A%2F%2Fdeeplink</div></td><td role="gridcell"><div class="slds-truncate" >2/10/2020 6:27:08 PM</div></td><td role="gridcell"><div class="slds-truncate" >2/10/2020 6:49:00 PM</div></td><td><div id="onelink-trigger274acd92-fa1a-4e81-b55d-ef663a8685c1" class="slds-dropdown-trigger slds-dropdown-trigger_click"><button class="slds-button slds-button_icon slds-button_icon-border-filled" aria-haspopup="true"><svg class="slds-button__icon" aria-hidden="true"><use xlink:href="/mcapp/images/symbols.svg#down"></use></svg><span class="slds-assistive-text">Show More</span></button><div class="slds-dropdown slds-dropdown_left"><ul class="slds-dropdown__list" role="menu" aria-label="Show More"><li class="slds-dropdown__item" role="presentation"><a href="/dashboard/edit/?lid=274acd92-fa1a-4e81-b55d-ef663a8685c1&eid={0}&rt={1}" class="edit" id="edit0" role="menuitem" tabindex="0"><span class="slds-truncate" title="Edit">Edit</span></a></li></ul></div></div></td></tr><table></div>';
