5df550e6d769a902002c6cb7e5fdf84b9888e54c
ntavora <ntavora@devsutd.com>
2020-06-05 16:22:04 -0300

    updates on links object

--- public/mcapp/js/htmlemailsDashborad.js
+++ public/mcapp/js/htmlemailsDashborad.js
@@ -27,7 +27,6 @@ function updateEmail(accessToken, emailId, EmailObject) {
   $.ajax({
     "url": "/sfmc/UpdateEmail",
     "method": "POST",
-    "timeout": 0,
     "headers": {
       "Content-Type": "application/json"
     },
@@ -73,21 +72,25 @@ function GetAllContentBuilderAssets(accessToken) {
 }
 
 
-function getLinks(rawHTML) {
+function getLinks(id, rawHTML) {
   var rawHTML = '<!DOCTYPE html><html><head> <title></title> <meta charset="utf-8"> <meta http-equiv="x-ua-compatible" content="ie=edge">  <meta name="viewport" content="width=device-width,initial-scale=1"> <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css"> <link rel="stylesheet" href="/mcapp/css/salesforce-lightning-design-system/assets/styles/salesforce-lightning-design-system.min.css"> <link href="/mcapp/css/create-style.css" rel="stylesheet" type="text/css"> <script type="text/javascript" src="/mcapp/js/jquery.min.js"></script> <style>::-webkit-input-placeholder{/* Chrome/Opera/Safari */ font-size: 13px; font-family: museo_sans300;}::-moz-placeholder{/* Firefox 19+ */ font-size: 13px; font-family: museo_sans300;}:-ms-input-placeholder{/* IE 10+ */ font-size: 13px; font-family: museo_sans300;}:-moz-placeholder{/* Firefox 18- */ font-size: 13px; font-family: museo_sans300;}#rules{margin-left: 30px;}#btn-addParameter{margin-left: 60px;}.rules-container>.af-button:before{content: ""; position: absolute; top: 50%; left: -1px; transform: translate(-100%, -50%); width: 22px; border: none; border-top: 2px solid #e8e8e8; z-index: 0;}.af-core.input-item .input-container .input-inner input, .af-core.input-item .input-container .input-inner textarea{/* font-family: museo_sans300; */ font-weight: 300; font-size: 13px; height: 36px; border-radius: 5px; border: 1px solid #7e848c; line-height: 1.14; color: #3f4a56; padding: 0 10px; font-family: museo_sans300;}.Select-value-label, #btn-addLink, #cancel, .af-button-text{font-family: museo_sans300; font-size: 13px;}.slds-form-element__label{font-family: museo_sans300; font-size: 15px;}.slds-form-element{width: 80%; padding-left: 6%; padding-top: 1%;}.hide{display: none !important;}.show{display: block !important;}.slds-box{background-color:pink;}.c3 svg, .datepicker-days, .dc-chart .tick text, .navbar-search .search-query, .panel-default .panel-heading, body, button, input, select, textarea{background-color: #fff; font-family: museo_sans300 !important; font-weight: 400; font-style: normal; color: #707070;}</style></head><body> <div> <a href="www.google.com"> google </a> <a href="www.facebook.com"> facebook </a> </div></body></html>';
   var doc = document.createElement("html");
   doc.innerHTML = rawHTML;
   var links = doc.getElementsByTagName("a")
-  console.log(links);
-  var urls = [];
+  var urls = {
+    Links: [],
+    EmailId: id
+  };
 
   for (var i = 0; i < links.length; i++) {
-    console.log(links[i].outerHTML);
-    var linkData= {
-      htmlLink: links[i].outerHTML,
-      link: links[i].getAttribute("href")
+    var LinkHtml = links[i].outerHTML;
+    var HtmlLinkText = LinkHtml.split('>')[1].split('<')[0].trim();
+    var linkData = {
+      htmlLink: LinkHtml,
+      LinkText: HtmlLinkText,
+      href: links[i].getAttribute("href")
     }
-    urls.push(linkData);
+    urls.Links.push(linkData);
   }
   return urls;
 }
@@ -98,17 +101,19 @@ function replaceUrlTOkens(token) {
   $('#DashboardLink')[0].href = '/Dashboard/home?rt=' + token + '&eid=' + $('#eid').val();
 }
 
-function replaceLinks(rawHTML, object) {  
+function replaceLinks(rawHTML, object) {
 
   for (var i = 0; i < object.length; i++) {
     console.log(object[i]);
     var oldString = object[i].oldLink.htmlLink;
-    var newString = oldString.replace(object[i].oldLink.link,object[i].newLink);
+    var linkText = oldString.split('>')[1].split('<')[0].trim();
+    var newString = oldString.replace(object[i].oldLink.link, object[i].newLink);
     console.log("oldString " + oldString);
     console.log("newString " + newString);
-    rawHTML.replace(oldString,newString);
+    //   rawHTML.replace(oldString.substring(oldString.indexOf('<'),oldString.indexOf('>') + 1), newString.substring(newString.indexOf('<'),newString.indexOf('>') + 1));
+    rawHTML.replace
   }
- return rawHTML;
+  return rawHTML;
 }
 
 /* eslint-disable no-undef */
@@ -122,7 +127,7 @@ function getUrlParameters() {
 }
 
 
-function buildDashboard(emails, from, page) {
+function href(emails, from, page) {
   let table = '<div class="slds-lookup" data-select="multi" data-scope="single" data-typeahead="true">';
   table += '<table class="slds-table slds-table_cell-buffer slds-no-row-hover slds-table_bordered slds-table_fixed-layout" role="grid" >';
 
@@ -134,27 +139,16 @@ function buildDashboard(emails, from, page) {
   table += '</tr>';
 
   if (emails !== undefined) {
- /*   emails.sort((a, b) => ((new Date(a.Modified) < new Date(b.Modified)) ? 1 : ((new Date(b.Modified) < new Date(a.Modified)) ? -1 : 0)));
-    if (from == "init" || page == 1) {
-      emails = emails.splice(0, 15);
-      $("#currentDashboard").val(15);
-    }
-    else {
-      var currentDashboard = $("#currentDashboard").val();
-      links = links.splice(currentDashboard, 15);
-      $("#currentDashboard").val(currentDashboard + 15);
-    }*/
-
     for (let index = 0; index < emails.length; index++) {
       const element = emails[index];
-       table += '<tr>';
+      table += '<tr>';
 
       table += `<td role="gridcell" colspan="2"><div class="slds-truncate" ><a href="#" id="link${index}">${element.name}</a> </div></td>`;
       table += `<td role="gridcell" colspan="2"><div class="slds-truncate" >${element.assetType.name}</div></td>`;
       table += `<td role="gridcell" colspan="3"><div class="slds-truncate" title="${element.owner.name}">${element.owner.name}</div></td>`;
       table += '</tr>';
     }
-  } 
+  }
   table += '</table>';
   table += '</div>';
 
@@ -168,18 +162,18 @@ function buildDashboard(emails, from, page) {
     document.getElementById(`link${index}`).addEventListener("click", function (e) {
       e.preventDefault();
       var rawHTML = element.views.html.content;
-      var links = getLinks(rawHTML);
-       console.log(links);
-       var arrObj=[]
-        for (let index = 0; index < links.length; index++) {
-          
-          arrObj.push({
-            oldLink: links[index],
-            newLink: "www.devsutd.com"
-         });
-          
-        }
-       console.log(replaceLinks(rawHTML,arrObj));
+      var links = getLinks(element.id, rawHTML);
+      console.log(links);
+      var arrObj = []
+      for (let index = 0; index < links.length; index++) {
+
+        arrObj.push({
+          oldLink: links[index],
+          newLink: "www.devsutd.com"
+        });
+
+      }
+      console.log(replaceLinks(rawHTML, arrObj));
     });
   }
 }
@@ -221,9 +215,9 @@ function loadHtmlEmails(urlParams, from, page) {
     data: postData,
     success: (data) => {
       var emails = data.body;
-            $('#rt').val(data.refresh_token);
-            replaceUrlTOkens($('#rt').val());
-            buildDashboard(emails.items, from, page);
+      $('#rt').val(data.refresh_token);
+      replaceUrlTOkens($('#rt').val());
+      buildDashboard(emails.items, from, page);
     },
     error(jqXHR, error, errorThrown) {
       const sat = '<div class="slds-lookup" data-select="multi" data-scope="single" data-typeahead="true"><table class="slds-table slds-table_cell-buffer slds-no-row-hover slds-table_bordered slds-table_fixed-layout" role="grid" ><thead><tr class="slds-line-height_reset"><th scope="col" colspan="2"><b>OneLink Name</b></th><th scope="col" colspan="2"><b>Full URL</b></th><th scope="col" ><b>URL</b></th><th scope="col" ><b># of Contents</b></th><th scope="col" ><b>Parameters</b></th><th scope="col" ><b>Custom Parameters</b></th><th scope="col" ><b>Created</b></th><th scope="col" ><b>Modified</b></th><th scope="col" ></th></tr></thead><tbody><tr><td role="gridcell" colspan="2"><div class="slds-truncate" >Security Review Test</div></td><td role="gridcell"  colspan="2"><div class="slds-truncate" title="https://af-esp.onelink.me/mYu3?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review&af_dp=esp%3A%2F%2Fdeeplink">https://af-esp.onelink.me/mYu3?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review&af_dp=esp%3A%2F%2Fdeeplink</div></td><td role="gridcell"><div class="slds-truncate" >https://af-esp.onelink.me/mYu3</div></td><td role="gridcell"><div class="slds-truncate" >1</div></td><td role="gridcell"><div class="slds-truncate" >?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review</div></td><td role="gridcell"><div class="slds-truncate" >&af_dp=esp%3A%2F%2Fdeeplink</div></td><td role="gridcell"><div class="slds-truncate" >2/10/2020 6:27:08 PM</div></td><td role="gridcell"><div class="slds-truncate" >2/10/2020 6:49:00 PM</div></td><td><div id="onelink-trigger274acd92-fa1a-4e81-b55d-ef663a8685c1" class="slds-dropdown-trigger slds-dropdown-trigger_click"><button class="slds-button slds-button_icon slds-button_icon-border-filled" aria-haspopup="true"><svg class="slds-button__icon" aria-hidden="true"><use xlink:href="/mcapp/images/symbols.svg#down"></use></svg><span class="slds-assistive-text">Show More</span></button><div class="slds-dropdown slds-dropdown_left"><ul class="slds-dropdown__list" role="menu" aria-label="Show More"><li class="slds-dropdown__item" role="presentation"><a href="/dashboard/edit/?lid=274acd92-fa1a-4e81-b55d-ef663a8685c1&eid={0}&rt={1}" class="edit" id="edit0" role="menuitem" tabindex="0"><span class="slds-truncate" title="Edit">Edit</span></a></li></ul></div></div></td></tr><tr><td role="gridcell" colspan="2"><div class="slds-truncate" >Security Review Test</div></td><td role="gridcell"  colspan="2"><div class="slds-truncate" title="https://af-esp.onelink.me/mYu3?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review&af_dp=esp%3A%2F%2Fdeeplink">https://af-esp.onelink.me/mYu3?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review&af_dp=esp%3A%2F%2Fdeeplink</div></td><td role="gridcell"><div class="slds-truncate" >https://af-esp.onelink.me/mYu3</div></td><td role="gridcell"><div class="slds-truncate" >1</div></td><td role="gridcell"><div class="slds-truncate" >?pid=Email-SFMC&af_channel=Salesforce Marketing Cloud&is_retargeting=true&c=security_review</div></td><td role="gridcell"><div class="slds-truncate" >&af_dp=esp%3A%2F%2Fdeeplink</div></td><td role="gridcell"><div class="slds-truncate" >2/10/2020 6:27:08 PM</div></td><td role="gridcell"><div class="slds-truncate" >2/10/2020 6:49:00 PM</div></td><td><div id="onelink-trigger274acd92-fa1a-4e81-b55d-ef663a8685c1" class="slds-dropdown-trigger slds-dropdown-trigger_click"><button class="slds-button slds-button_icon slds-button_icon-border-filled" aria-haspopup="true"><svg class="slds-button__icon" aria-hidden="true"><use xlink:href="/mcapp/images/symbols.svg#down"></use></svg><span class="slds-assistive-text">Show More</span></button><div class="slds-dropdown slds-dropdown_left"><ul class="slds-dropdown__list" role="menu" aria-label="Show More"><li class="slds-dropdown__item" role="presentation"><a href="/dashboard/edit/?lid=274acd92-fa1a-4e81-b55d-ef663a8685c1&eid={0}&rt={1}" class="edit" id="edit0" role="menuitem" tabindex="0"><span class="slds-truncate" title="Edit">Edit</span></a></li></ul></div></div></td></tr><table></div>';
